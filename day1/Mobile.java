4e0b02a3adc845178d7aa4f1b2598dd6ca7a8dc1
AparnaBalakrishnan <123627482+AparnaBalakrishnan@users.noreply.github.com>
2023-01-26 15:50:20 +0530

    Add files via upload
    Day1 Assignments

---
+++ day1/Mobile.java
@@ -0,0 +1,61 @@
+package week1.day1;
+
+public class Mobile {
+	public void call() {
+		System.out.println("Call to Bhuvana");
+	}
+	public void sendMessage() {
+		System.out.println("Send Message To Srimathi");
+	}
+	public void shareFile() {
+		System.out.println("Share The File");
+	}
+	public void mobileCharging() {
+		System.out.println("Charge The Mobile");
+	}
+	public void listeningSong() {
+		System.out.println("Listening Songs");
+	}
+	public void watchingMovie() {
+		System.out.println("Watching Movie");
+	}
+	public void takePhotos() {
+		System.out.println("Taking Photos");
+	}
+	public void installApps() {
+		System.out.println("Installing the Applications");
+	}
+	public void downloadFiles() {
+		System.out.println("Download The Files");
+	}
+	public void playgames() {
+		System.out.println("Playing Games");
+	}
+	public void applySim() {
+		System.out.println("Apply Sim");
+	}
+	public void putBattery() {
+		System.out.println("Put Bettery For Mobile");
+	}
+	public void mobileRecharge() {
+		System.out.println("Recharge The Mobile Data");
+	}
+	public static void main(String[] args) {
+		Mobile ob = new Mobile();
+		ob.call();
+		ob.sendMessage();
+		ob.shareFile();
+		ob.mobileCharging();
+		ob.listeningSong();
+		ob.watchingMovie();
+		ob.takePhotos();
+		ob.installApps();
+		ob.downloadFiles();
+		ob.playgames();
+		ob.applySim();
+		ob.putBattery();
+		ob.mobileRecharge();
+	
+	}
+
+}
---
+++ day1/car.java
@@ -0,0 +1,56 @@
+package week1.day1;
+
+public class car {
+	public void drivecar() {
+		System.out.println("driver the car");
+	}
+	public void applybreak() {
+		System.out.println("Apply break");
+	}
+	public void soundhound() {
+		System.out.println("Sound Hound");
+	}
+	public void applyGear() {
+		System.out.println("Apply Gear");
+	}
+	public void applyAcclerate() {
+		System.out.println("Apply Acclerate");
+	}
+	public void switchOnAc() {
+		System.out.println("Switch on the AC");
+	}
+	public void applyIndicator() {
+		System.out.println("Apply Left Indicator and Right Indicator");
+	}
+	public void switchOnHeadlight() {
+		System.out.println("Switch on the Head Light");
+	}
+	public void putSeatBelt() {
+		System.out.println("Put The Seat Belt");
+	}
+	public void openSunroof() {
+		System.out.println("Open The Sunroof");
+	}
+	public void applyWiper() {
+		System.out.println("Apply wiper");
+	}
+	public void openPetrolTank() {
+		System.out.println("Open The Petrol Tank");
+	}
+	public static void main(String[] args) {
+	
+		car ob = new car(); 
+		ob.drivecar();
+		ob.applybreak();
+		ob.soundhound();
+		ob.applyGear();
+		ob.applyAcclerate();
+		ob.switchOnAc();
+		ob.applyIndicator();
+		ob.switchOnHeadlight();
+		ob.putSeatBelt();
+		ob.openSunroof();
+		ob.applyWiper();
+		ob.openPetrolTank();
+	}  
+}
